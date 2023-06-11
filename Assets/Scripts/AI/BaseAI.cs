a80b9fad5411d06d47a5ae6b57690945e63e9073
Magn1tola <kurqwertyar@gmail.com>
2023-06-11 23:15:13 +0400

    sdf

--- Assets/Scripts/AI/BaseAI.cs
+++ Assets/Scripts/AI/BaseAI.cs
@@ -4,5 +4,47 @@ using UnityEngine;
 
 public class BaseAI : BaseCharacter
 {
-    
+    protected GameObject player;
+    [SerializeField] protected float moveSpeed = 10f;
+    [SerializeField] protected float cooldownTime = 10f;
+    protected float currentCooldown = 0;
+
+    protected override void Start()
+    {
+        base.Start();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    protected virtual void Update()
+    {
+        UpdateCooldown();
+    }
+
+    protected virtual void Cooldown()
+    {
+        currentCooldown = cooldownTime;
+    }
+    protected virtual void UpdateCooldown()
+    {
+        if (currentCooldown > 0)
+        {
+            currentCooldown -= Time.deltaTime;
+        }
+        if (currentCooldown < 0)
+        {
+            currentCooldown = 0f;
+        }
+    }
+    protected virtual void MoveToTargetPosition(Vector2 position)
+    {
+        float directionX = (position.x > transform.position.x) ? 1f : -1f;
+        rigidBody2D.velocity = new Vector2(moveSpeed * directionX, rigidBody2D.velocity.y);
+        Flip();
+    }
+
+    protected override void Attack()
+    {
+        base.Attack();
+        Cooldown();
+    }
 }
--- Assets/Scripts/AI/Monsters/Skeleton.cs
+++ Assets/Scripts/AI/Monsters/Skeleton.cs
@@ -4,33 +4,33 @@ using UnityEngine;
 public class Skeleton : BaseAI
 {
     private Animator animator;
-
-
     protected override void Start()
     {
         base.Start();
         animator = GetComponent<Animator>();
     }
 
-    private void Update()
+    protected override void Update()
     {
-        
+        base.Update();
+        animator.SetFloat("Speed", Mathf.Abs(rigidBody2D.velocity.x));
+
+        if (Vector2.Distance(player.transform.position, transform.position) > attackDistance * 0.9f && currentCooldown == 0)
+            MoveToTargetPosition(player.transform.position);
+        else if (currentCooldown == 0)
+            animator.SetTrigger("Attack");
+
     }
+
     protected override void ApplyDamage(float damage, GameObject instigator)
     {
         base.ApplyDamage(damage, instigator);
         animator.SetTrigger("Damage");
     }
 
-    protected override void Attack()
-    {
-        base.Attack();
-        animator.SetTrigger("Attack");
-    }
-
     protected override void Dead()
     {
         animator.SetTrigger("Dead");
-        Destroy(gameObject, 3f);
+        Destroy(gameObject, 2f);
     }
 }
