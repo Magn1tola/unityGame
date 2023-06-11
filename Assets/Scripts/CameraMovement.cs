369ab18402c7a826bfc991ac2cec25feb01f5424
Magn1tola <kurqwertyar@gmail.com>
2023-06-12 00:02:43 +0400

    Added Smooth Camera Movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject _target;
+    [SerializeField] private float speed = 3f;
+
+    private Rigidbody2D _rigidbody2D;
+    private bool canMoving;
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+
+        if (_target == null)
+            _target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        if (canMoving)
+            _rigidbody2D.velocity = (_target.transform.position - transform.position) * speed;
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject == _target)
+            canMoving = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject != _target)
+            return;
+
+        canMoving = false;
+        _rigidbody2D.velocity = new Vector2(0f, 0f);
+    }
+}
