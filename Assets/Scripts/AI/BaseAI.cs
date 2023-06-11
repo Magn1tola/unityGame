42060866af860463789c0a0196ffd379b6de299a
Magn1tola <kurqwertyar@gmail.com>
2023-06-11 20:13:16 +0400

    hkol

--- Assets/Scripts/AI/BaseAI.cs
+++ Assets/Scripts/AI/BaseAI.cs
@@ -2,11 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(AIBehavior))]
 public class BaseAI : BaseCharacter
 {
-    private AIBehavior aiBehavior;
-    private void Awake() {
-        aiBehavior = GetComponent<AIBehavior>();
-    }
+    
 }
